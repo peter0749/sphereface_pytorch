e7ae3487be0439b9280468db545c8109768951c6
jengku <jengku@gmail.com>
2019-03-12 18:33:24 +0800

    try am-softmax

--- net_sphere.py
+++ net_sphere.py
@@ -5,13 +5,8 @@ import torch.nn.functional as F
 from torch.nn import Parameter
 import math
 
-def myphi(x,m):
-    x = x * m
-    return 1-x**2/math.factorial(2)+x**4/math.factorial(4)-x**6/math.factorial(6) + \
-            x**8/math.factorial(8) - x**9/math.factorial(9)
-
 class AngleLinear(nn.Module):
-    def __init__(self, in_features, out_features, m = 4, phiflag=True):
+    def __init__(self, in_features, out_features, m = 0.35, s = 30):
         super(AngleLinear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -19,41 +14,19 @@ class AngleLinear(nn.Module):
         self.weight.data.uniform_(-1, 1).renorm_(2,1,1e-5).mul_(1e5)
         self.phiflag = phiflag
         self.m = m
-        self.mlambda = [
-            lambda x: x**0,
-            lambda x: x**1,
-            lambda x: 2*x**2-1,
-            lambda x: 4*x**3-3*x,
-            lambda x: 8*x**4-8*x**2+1,
-            lambda x: 16*x**5-20*x**3+5*x
-        ]
+        self.s = s
 
     def forward(self, input):
-        x = input   # size=(B,F)    F is feature len
+        x = input   # x is a normalized feature vector, size=(B,F)    F is feature len
         w = self.weight # size=(F,Classnum) F=in_features Classnum=out_features
 
         ww = w.renorm(2,1,1e-5).mul(1e5)
-        xlen = x.pow(2).sum(1).pow(0.5) # size=B
-        wlen = ww.pow(2).sum(0).pow(0.5) # size=Classnum
 
         cos_theta = x.mm(ww) # size=(B,Classnum)
-        cos_theta = cos_theta / xlen.view(-1,1) / wlen.view(1,-1)
         cos_theta = cos_theta.clamp(-1,1)
+        phi_theta = cos_theta - self.m
 
-        if self.phiflag:
-            cos_m_theta = self.mlambda[self.m](cos_theta)
-            theta = Variable(cos_theta.data.acos())
-            k = (self.m*theta/3.14159265).floor()
-            n_one = k*0.0 - 1
-            phi_theta = (n_one**k) * cos_m_theta - 2*k
-        else:
-            theta = cos_theta.acos()
-            phi_theta = myphi(theta,self.m)
-            phi_theta = phi_theta.clamp(-1*self.m,1)
-
-        cos_theta = cos_theta * xlen.view(-1,1)
-        phi_theta = phi_theta * xlen.view(-1,1)
-        output = (cos_theta,phi_theta)
+        output = (cos_theta*self.s,phi_theta*self.s)
         return output # size=(B,Classnum,2)
 
 
@@ -61,13 +34,8 @@ class AngleLoss(nn.Module):
     def __init__(self, gamma=0):
         super(AngleLoss, self).__init__()
         self.gamma   = gamma
-        self.it = 0
-        self.LambdaMin = 5.0
-        self.LambdaMax = 1500.0
-        self.lamb = 1500.0
 
     def forward(self, input, target):
-        self.it += 1
         cos_theta,phi_theta = input
         target = target.view(-1,1) #size=(B,1)
 
@@ -76,10 +44,8 @@ class AngleLoss(nn.Module):
         index = index.byte()
         index = Variable(index)
 
-        self.lamb = max(self.LambdaMin,self.LambdaMax/(1+0.1*self.it ))
         output = cos_theta * 1.0 #size=(B,Classnum)
-        output[index] -= cos_theta[index]*(1.0+0)/(1+self.lamb)
-        output[index] += phi_theta[index]*(1.0+0)/(1+self.lamb)
+        output[index] = phi_theta[index] * 1.0
 
         logpt = F.log_softmax(output, dim=-1)
         logpt = logpt.gather(1,target)
@@ -170,6 +136,7 @@ class sphere20a(nn.Module):
 
         x = x.view(x.size(0),-1)
         x = self.fc5(x)
+        x = x / x.pow(2).sum(1).pow(0.5).view(-1,1) ## normalized embedding feature
         if self.feature: return x
 
         x = self.fc6(x)
